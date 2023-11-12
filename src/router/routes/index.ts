2a878115bfa19206b90816e30b33b36c52370859
ZLENGANG <1437004632@qq.com>
2023-11-13 00:15:38 +0800

    feature(custom): 增加随机股票

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -3,4 +3,5 @@ import '@/views/index/index.vue';
 export const baseRoutes = [
   { path: '/', component: () => import('@/views/index/index.vue') },
   { path: '/login', component: () => import('@/views/login/index.vue') },
+  { path: '/stock', component: () => import('@/views/stock/index.vue') },
 ];
