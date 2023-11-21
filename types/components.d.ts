93ccf2b4190b75617f6defa5d9fc9f31102b9ab0
ZLENGANG <1437004632@qq.com>
2023-11-21 23:32:29 +0800

    feature(custom): 主题

--- types/components.d.ts
+++ types/components.d.ts
@@ -13,6 +13,7 @@ declare module 'vue' {
     Loading: typeof import('./../src/components/loading/index.vue')['default']
     NButton: typeof import('naive-ui')['NButton']
     NCheckbox: typeof import('naive-ui')['NCheckbox']
+    NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NForm: typeof import('naive-ui')['NForm']
     NFormItem: typeof import('naive-ui')['NFormItem']
     NIcon: typeof import('naive-ui')['NIcon']
