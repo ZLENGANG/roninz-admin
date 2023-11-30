f92586820d294c71f83481c157e1700fc9a80af6
Roninz <859444366@qq.com>
2023-11-30 14:14:39 +0800

    feature(custom): 增加内嵌链接

---
+++ src/components/common/app-iframe.vue
@@ -0,0 +1,12 @@
+<template>
+  <iframe :src="link" frameborder="0" class="wh-full"></iframe>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+const route = useRoute();
+
+const link = route.meta.innerLink;
+</script>
+
+<style scoped></style>
