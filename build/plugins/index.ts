032cb99e918f44f985bff734f546afae1d6d3731
ZLENGANG <1437004632@qq.com>
2023-12-25 23:40:04 +0800

    feature(custom): 增加本地icon功能

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -8,9 +8,13 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 export const createVitePlugins = (env: ViteEnv, isBuild: boolean) => {
   const plugins = [
     vue(),
+    // 自动导入插件
     ...autoImport,
+    // html插件，给index.html注入变量
     configHtmlPlugin(env, isBuild),
+    // 原子化css插件
     Unocss(),
+    // 顶层await插件，解决打包编译出错问题
     topLevelAwait({
       promiseExportName: '__tla',
       promiseImportName: (i) => `__tla_${i}`,
